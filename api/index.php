869ebbff19f52e80394d9eaa38fd62385dda84ad
Bintang Permana <bintang.permana@dhealth.co.id>
2025-01-19 21:15:21 +0700

    first commit

--- api/index.php
+++ api/index.php
@@ -1,3 +1,17 @@
 <?php
 
-require __DIR__ . '/../public/index.php';
+// Menggunakan bootstrap Laravel tanpa perlu masuk ke public/index.php
+require __DIR__ . '/../vendor/autoload.php';
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+// Menjalankan aplikasi Laravel
+$kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+// Mengirim response ke browser
+$response->send();
+
+// Menyelesaikan request
+$kernel->terminate($request, $response);
